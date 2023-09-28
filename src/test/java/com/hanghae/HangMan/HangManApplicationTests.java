e9a0e4d37284c79164207aa2decb24e6c96a10ee
LeeJeongGyo <ella.code.j@gmail.com>
2023-09-28 23:16:29 +0900

    Create:최초 파일

---
+++ src/test/java/com/hanghae/HangMan/HangManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.HangMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
